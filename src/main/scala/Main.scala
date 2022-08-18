2d965a1b74c84a6a92abadabb1e3b11a611c6b38
Pratyush Das <reikdas@gmail.com>
2022-08-18 12:27:01 -0400

    Push local changes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,40 +1,22 @@
-import lms.collection.mutable.ArrayOps
 import lms.core.stub._
 import lms.macros.SourceContext
-import lms.core.virtualize
-import lms.thirdparty.{LibFunction, LibStruct}
+import lms.core.{Backend, virtualize}
+import lms.thirdparty.{CCodeGenLibFunction, LibFunction, ScannerOps}
 
-import sys.process._
+import java.io.File
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
-import scala.io.StdIn.readLine
+import sys.process._
 
-trait FileOps extends Base with ArrayOps with LibStruct with LibFunction {
-  def open(loc: Rep[String]): Rep[Int] = libFunction[Int]("open", Unwrap(loc), lms.core.Backend.Const(0))(Seq(), Seq(), Set())
-
-  def filelen(fd: Rep[Int])(implicit o: Overloaded1): Rep[Long] = uncheckedPure[Long]("fsize(", fd, ")") // FIXME: Use libFunction instead
-  def filelenf(fd: Rep[Int]) = uncheckedPure[Int]("fsize(", fd, ")") // FIXME: Use libFunction instead
-  def mmap[T](fd: Rep[Int], len: Rep[Long])(implicit o: Overloaded2, mt: Manifest[T]): Rep[LongArray[T]] = Wrap[LongArray[T]](Adapter.g.reflect("mmap", Unwrap(len), Unwrap(fd)))
-  def mmap[T](fd: Rep[Int], len: Rep[Int])(implicit o: Overloaded1, mt: Manifest[T]): Rep[Array[T]] = Wrap[Array[T]](Adapter.g.reflect("mmap", Unwrap(len), Unwrap(fd)))
-  def mmapf[T:Manifest](fd: Rep[Int], len: Rep[Int]) = mmap(fd, len)
-  def stringFromCharArray(data: Rep[LongArray[Char]], pos: Rep[Long]): Rep[String] = uncheckedPure[String](data," + ",pos) // FIXME: Use libFunction instead
-
-  class hdfsFS
-  def connectToHDFS(address: Rep[String], port: Rep[Int]): Rep[hdfsFS] = libFunction[hdfsFS]("hdfsConnect", Unwrap(address), Unwrap(port))(Seq(0, 1), Seq(), Set())
-  class hdfsFile
-  def HDFSOpenFile(fs: Rep[hdfsFS], path: Rep[String], flag: Rep[Int], bufferSize: Rep[Int], replication: Rep[Int], blockSize: Rep[Int]): Rep[hdfsFile] = libFunction[hdfsFile]("hdfsOpenFile", Unwrap(fs), Unwrap(path), Unwrap(flag), Unwrap(bufferSize), Unwrap(replication), Unwrap(blockSize))(Seq(), Seq(), Set())
-  class hdfsAvailable
-  def hdfsAvailable(fs: Rep[hdfsFS], readFile: Rep[hdfsFile]): Rep[Long] = libFunction[Long]("hdfsAvailable", Unwrap(fs), Unwrap(readFile))(Seq(), Seq(), Set())
-  def malloc[T](availsize: Rep[Long]): Rep[LongArray[Char]] = uncheckedPure[LongArray[Char]]("malloc(sizeof(char)*(", availsize, "+1))") // FIXME: Use libFunction instead
-  def memset(buf: Rep[LongArray[Char]]) = unchecked("memset(", buf, ", 0, sizeof(", buf, "))") // FIXME: Use libFunction instead
-  def readBytes(fs: Rep[hdfsFS], readFile: Rep[hdfsFile], pos: Rep[Long], avail: Rep[Long], buf: Rep[LongArray[Char]]): Rep[Long] = unchecked[Long]("hdfsPread(", fs, ",", readFile, ",", pos, ", &", buf, "[", pos, "], ", avail, ")") // FIXME: Use libFunction instead
-  def disconnectHDFS(handle: Rep[hdfsFS]): Rep[Int] = unchecked("hdfsDisconnect(", handle, ")") // FIXME: Use libFunction instead
-  def closeHDFS(handle: Rep[hdfsFS], openFile: Rep[hdfsFile]): Rep[Int] = unchecked("hdfsCloseFile(", handle, ", ", openFile, ")") // FIXME: Use libFunction instead
+trait FileOps extends ScannerOps with LibFunction {
+  def read(fd: Rep[Int], buf: Rep[Array[Char]], size: Rep[Long]): Rep[Int] = libFunction[Int]("read", Unwrap(fd), Unwrap(buf), Unwrap(size))(Seq(0, 1, 2), Seq(), Set(), Adapter.CTRL)
 }
 
-@virtualize
-object Main extends App {
 
-  /*def GetPath(path: String): Unit = {
+object Main {
+
+  def GetPaths(path: String): ListBuffer[String] = {
+    val basepath = "/home/reikdas/hdfs-data/datanode" // Change as required
     val result = "hdfs fsck %s -files -blocks -locations".format(path)
     val output = result.!!
     val lines = output.split("\n")
@@ -78,20 +60,108 @@ object Main extends App {
     }
     assert(size > 0)
     assert(blocks_infos.size == dnodes_infos.size)
-    println("Size = ", size)
-    for (i <- blocks_infos.indices) {
-      println("current/%s/current/finalized/subdir0/subdir0/%s".format(dnodes_infos(i), blocks_infos(i)))
-      println("IP = ", ips_infos(i))
+    val nativepaths = new ListBuffer[String]
+
+    def getListOfFiles(dir: File): Array[File] = {
+      val these = dir.listFiles
+      these ++ these.filter(_.isDirectory).flatMap(getListOfFiles)
     }
-  }*/
 
+    for (i <- blocks_infos.indices) {
+      val allpaths = getListOfFiles(new File(basepath + "/current/%s/current/finalized".format(dnodes_infos(i))))
+      var flag = 0
+      for (j <- allpaths.indices) {
+        if (allpaths(j).toString.contains(blocks_infos(i)) && !allpaths(j).toString.contains(".meta") && flag == 0) {
+          nativepaths += allpaths(j).toString
+          flag = 1
+        }
+      }
+      if (flag == 0)
+        throw new Exception("Should be unreachable")
+    }
+    nativepaths
+  }
 
-  val snippet = new DslDriverC[Int, Unit] {
-    // val paths = GetPath(..)
-    val paths = List("/foo.txt", "/bar.txt")
-    def snippet(dummy: Rep[Int]) = {
-      for (i <- 0 until paths.length: Range) println(paths(i))
+  def main(args: Array[String]): Unit = {
+    val snippet = new DslDriverC[Int, Unit] with FileOps {
+      q =>
+      override val codegen = new DslGenC with CCodeGenLibFunction {
+        val IR: q.type = q
+      }
+
+      //val paths = GetPaths("/200M.txt")
+      val paths = List("foo.txt", "bar.txt")
+
+      @virtualize
+      def snippet(dummy: Rep[Int]) = {
+        var count = 0L
+        val buf = NewArray[Char](1000000000)
+        for (i <- 0 until paths.length: Range) {
+          val block_num = open(paths(i))
+          val size = filelen(block_num)
+          val toread = read(block_num, buf, size)
+          for (j <- 0 until toread) {
+            if (buf(j) != ' ') count += 1
+          }
+        }
+        println(count)
+      }
+    }
+
+    println(snippet.code)
+  }
+
+  /*
+
+  def verifyChecksum(blocks: ListBuffer[String], filename: String): Unit = {
+
+    val localhash = {
+      val sb = new mutable.StringBuilder("")
+      for (i <- blocks.indices) {
+        sb ++= blocks(i)
+        sb ++= " "
+      }
+      val localcheckcom = "md5sum %s | md5sum".format(sb)
+      println(localcheckcom)
+      val localcheck = localcheckcom.!!
+      localcheck
+    }
+
+    val hdfshash = {
+
     }
   }
-  println(snippet.code)
+
+
+  val filename = "/1G.txt"
+  val blocks = GetPaths(filename)
+  verifyChecksum(blocks, filename)
+
+  def HDFSfilelen(filename: String): Long = {
+
+  }
+
+  def GetBlockLen(): Long = {
+  }
+
+  def read(fd: Int, buf: List[Char], start: Int, end: Int): Unit = {
+
+  }
+
+  def HDFSread(filename: String, start: Int, buflen: Long): Rep[List[Char]] = {
+    val blockpaths = GetBlocks(filename)
+    val filesize: Long = HDFSfilelen(filename)
+    val blocksize: Long = GetBlockLen()
+    val blocknum: Int = filesize/blocksize
+    var pos: Rep[Int] = var_new(filesize % blocksize)
+    val buf: Rep[List[Char]] = NewArray[Char](buflen)
+    var readsize: Rep[Long] = var_new(0: Long)
+    var remaining: Rep[Long] = var_new(buflen)
+    while (true) {
+      val fd: Rep[Int]  = open(paths(i))
+      if (remaining > )
+      read(fd, buf, start)
+    }
+    buf
+    }*/
 }
