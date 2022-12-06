7bf722945a6ff62caa5fefc054bf73d164f46c62
Pratyush Das <reikdas@gmail.com>
2022-12-06 17:41:45 -0500

    Updated benchmarks for all versions

--- bench.py
+++ bench.py
@@ -12,18 +12,20 @@ def find_between(s, start, end):
 if __name__ == "__main__":
     conf = {
             "/1G.txt": [1, 2, 4, 8],
-            "/10G.txt": [1, 2, 4, 8, 16, 32, 64, 79],
+#             "/10G.txt": [1, 2, 4, 8, 16, 32, 64, 79],
             }
+    classes = ["WordCount", "CharFreq", "Whitespace"]
     includeFlags = "-I {0}/src/main/resources/headers/ -I {1}/src/main/resources/headers/".format(lmshdfs_path, lms_path)
-    for key, values in conf.items():
-        with open("bench{0}".format(key[1:]), "w") as f:
-            subprocess.run(shlex.split("sbt \"run --loadFile={0} --writeFile=bench.c --bench\"".format(key)))
-            subprocess.run(shlex.split("mpicc -O3 bench.c {0} -o bench".format(includeFlags)))
-            for value in values:
-                for i in range(5): # Get the program in cache
-                    output = subprocess.run(shlex.split("mpirun -np {0} -map-by numa bench 0".format(value)), capture_output=True)
-                output = output.stdout.decode("utf-8")
-                times = []
-                for line in output.split("\n")[:-1]:
-                    times.append(float(find_between(line, "spent", "time").strip()))
-                f.write(str(value) + "," + str(mean(times)) + "\n")
+    for scalaclass in classes:
+        for key, values in conf.items():
+            with open("bench{0}{1}".format(scalaclass, key[1:]), "w") as f:
+                subprocess.run(shlex.split("sbt \"runMain {0} --loadFile={1} --writeFile=bench.c --bench\"".format(scalaclass, key)))
+                subprocess.run(shlex.split("mpicc -O3 bench.c {0} -o bench".format(includeFlags)))
+                for value in values:
+                    for i in range(5): # Get the program in cache
+                        output = subprocess.run(shlex.split("mpirun -np {0} -map-by numa bench 0".format(value)), capture_output=True)
+                    output = output.stdout.decode("utf-8")
+                    times = []
+                    for line in output.split("\n")[:-1]:
+                        times.append(float(find_between(line, "spent", "time").strip()))
+                    f.write(str(value) + "," + str(mean(times)) + "\n")
