7fd67ee7a134611258eabe7fc209b87b4c27bf72
Pratyush Das <reikdas@gmail.com>
2023-01-17 10:47:14 -0500

    Modify benchmarking script a little

--- bench/benchC.py
+++ bench/benchC.py
@@ -5,25 +5,24 @@ from pathlib import Path
 from statistics import mean
 
 root_path = Path(__file__).resolve().parent.parent.absolute()
-lms_path = os.path.join(lmshdfs_path, "lms-clean")
+lms_path = os.path.join(root_path, "lms-clean")
 
 if __name__ == "__main__":
     filenames = ["1G.txt", "10G.txt", "50G.txt", "100G.txt", "200G.txt"]
     classes = ["wc", "cf", "ws"]
-    # classes = ["wc"]
     includeFlags = "-I {0}/src/main/resources/headers/ -I {1}/src/main/resources/headers/".format(root_path, lms_path)
     for scalaclass in classes:
         with open(os.path.join(root_path, "bench", "benchC{0}.csv".format(scalaclass)), "w") as f:
-            f.write("file,mmap,read\n")
-            # f.write("file,mmap\n")
+            #f.write("file,mmap,read\n")
+            f.write("file,mmap\n")
             print("Benchmarking {0}".format(scalaclass))
             subprocess.run(shlex.split("gcc -O3 canon{0}mmap.c {1} -o benchcanon{0}mmap".format(scalaclass, includeFlags)))
-            subprocess.run(shlex.split("gcc -O3 canon{0}read.c {1} -o benchcanon{0}read".format(scalaclass, includeFlags)))
+            #subprocess.run(shlex.split("gcc -O3 canon{0}read.c {1} -o benchcanon{0}read".format(scalaclass, includeFlags)))
             for filename in filenames:
                 f.write(filename)
                 print("For {0}".format(filename))
-                for rformat in ["mmap", "read"]:
-                # for rformat in ["mmap"]:
+                #for rformat in ["mmap", "read"]:
+                for rformat in ["mmap"]:
                     testout = subprocess.run(shlex.split("./benchcanon{0}{1} /scratch1/das160/{2}".format(scalaclass, rformat, filename)), capture_output=True)
                     # testout = testout.stdout.decode("utf-8")
                     # testout = testout[testout.index("\n") + 1:]
